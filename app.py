929ea8e92c2f1644a1eed24cdbfee7dcd37d4dca
0xVanes <vansart10@gmail.com>
2026-06-24 16:42:50 +0700

    added connection to n8n

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import streamlit as st
+import requests
 from dotenv import load_dotenv, find_dotenv
 from openai import OpenAI
 load_dotenv(find_dotenv(), override=True)
+
+st.set_page_config(page_title="FinPRO-JOB 👩🏻‍💻🧑🏻‍💻💼🔎", layout="wide")
+st.title("FinPRO-JOB")
+
+n8n_url = "https://n8n-student.purwadhika.com/webhook/finproJOB"
+respond = requests.post(n8n_url)
+print(respond.status_code)
+print(dict(respond.headers))
+print(respond.text)
