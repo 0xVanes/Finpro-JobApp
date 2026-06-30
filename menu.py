5f45a5424dcb1ef724d10239b2e601b5b4e44731
0xVanes <vansart10@gmail.com>
2026-06-30 10:50:08 +0700

    update css and add css to each files in pages folder

--- menu.py
+++ menu.py
@@ -1,12 +1,34 @@
 import streamlit as st
 
+## Loading CSS
+def load_css():
+    with open("styles.css", "r") as f:
+        css = f.read()
+    st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+load_css()
+
 def authenticated_menu():
     """Show navigation menu for authenticated users"""
     st.sidebar.header(f"👋 Welcome, {st.session_state.username}!")
     st.sidebar.caption(f"Role: {st.session_state.role}")
+
+    st.sidebar.markdown(f"""
+<div style="display:flex;align-items:center;gap:12px;margin-bottom:15px;">
+    <div style="width:48px; height:48px; border-radius:14px;
+        background:linear-gradient(135deg,#7C6CFF,#59D6D6);
+        color:white; display:flex; align-items:center; justify-content:center;
+        font-size: 30px; font-weight:900;">JP
+    </div>
+    <div>
+        <div style = "font-size:20px;"> <b> JobPath </b> </div>
+        <div> Career Navigator </div>
+    </div>
+    
+</div> """, unsafe_allow_html=True)
+
     st.sidebar.divider()
     
-    st.sidebar.header("JobPath")
+    st.sidebar.header("Menu")
     
     # Using page_link
     st.sidebar.page_link("pages/1job_chat.py", label="💬 Job Chat")
--- pages/5skill_gap.py
+++ pages/5skill_gap.py
@@ -6,3 +6,10 @@ st.set_page_config(page_title="FinPRO-JOB - Dashboard", layout="wide")
 
 # Load menu
 menu_with_redirect()
+
+## Loading CSS
+def load_css():
+    with open("styles.css", "r") as f:
+        css = f.read()
+    st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+load_css()
